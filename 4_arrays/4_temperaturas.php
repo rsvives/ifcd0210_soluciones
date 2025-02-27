8ad323b332a436d6bc73ad7c6adbf31fdedbcf02
Rodri <r.serranvives@gmail.com>
2025-02-27 18:56:20 +0100

    enunciado ejercicio temperaturas

---
+++ 4_arrays/4_temperaturas.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+Dado un array con la temperatura media de  cada día de la semana, sacar:
+ - la temperatura máxima, 
+ - la mínima 
+ - y la temperatura media
+
+*/
+
+
+$temperaturas = [12, 15, 13, 12, 16, 11, 13];
