629537965b94e6d2abd41ef0ecde2baa7afc0b45
Rodri <r.serranvives@gmail.com>
2025-02-27 18:30:08 +0100

    estoy haciendo una prueba

--- 4_arrays/3_agenda.php
+++ 4_arrays/3_agenda.php
@@ -10,14 +10,15 @@ una vez hecho, mostrar el texto "X tiene el número 6666666"
 
 */
 
-$agenda=[
-    "pelayo"=>666666666,
+$agenda = [
+    "pelayo" => 666666666,
     "mónica" => 55555555,
-    "guillermo" =>444455555,
-    "marta" =>876876876,
+    "guillermo" => 444455555,
+    "marta" => 876876876,
 ];
 
 foreach ($agenda as $nombre => $tfono) {
-    echo("$nombre : $tfono");
+    echo ("$nombre : $tfono");
     echo "<br>";
 }
+//aqui voy a añadir una linea de prueba
