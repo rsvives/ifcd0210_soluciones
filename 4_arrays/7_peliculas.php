77473f1296a827571d1557116ca67314dd03c5c1
Rodri <r.serranvives@gmail.com>
2025-02-28 18:26:49 +0100

    pelis 🎬

---
+++ 4_arrays/7_peliculas.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+    Crear un objeto(array asociativo) que represente una película. 
+    Para ello ha de tener lo siguientes campos:
+        - titulo
+        - año
+        - director
+        - genero
+
+
+    Una vez creado un objeto, intentar crear un array con 3 películas, 
+    iterarlas para mostrarlas en 3 divs html con sus correspondientes atributos
+*/
