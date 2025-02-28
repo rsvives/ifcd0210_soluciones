96893676f0b0feca7a15e02f5fb3a25cc9c38dd2
Rodri <r.serranvives@gmail.com>
2025-02-28 11:08:53 +0100

    ejercicios viernes

---
+++ 4_arrays/5_temperaturas-semana.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+    Crear un array $dias con las siguientes claves: "lunes","martes", ..., "sábado", "domingo"
+
+    Iterando ese array, crear otro array asociativo $temperaturas:
+        - cuya clave es el día de la semana
+        - el valor es un número aleatorio 
+
+    Iterar $temperaturas, mostrando el texto "la temperatura del lunes es X ºC, ..."
+
+    Pista: el método push() puede resultar útil para añadir elementos a un array nuevo
+*/
