f3b99ffdcfe9c94bf57bc1c361119c345dd58a74
rbercianos-class <rbifcd0210@gmail.com>
2025-03-03 20:26:59 +0100

    funciones resueltas

--- 5_funciones/1_hola_mundo.php
+++ 5_funciones/1_hola_mundo.php
@@ -2,3 +2,12 @@
 /* 
     Crear una función que haga echo de "hola mundo!". Probar a llamarla múltiples veces
 */
+function saludar (){
+    echo "hola mundo!";
+}
+
+for ($i=0; $i < 4; $i++) { 
+    saludar();
+    echo "<br>";
+}
+?>
--- 5_funciones/4_calculadora.php
+++ 5_funciones/4_calculadora.php
@@ -9,3 +9,49 @@
     Si la operación no es ninguna de las anteriores, se ha de mostrar un mensaje de error: "Operación desconocida"
 
 */
+
+function sumar($num1, $num2)  {
+    return $num1+$num2;
+}
+
+function restar($num1, $num2)  {
+    return $num1-$num2;
+}
+
+function multiplicar($num1, $num2)  {
+    return $num1*$num2;
+}
+
+function dividir($num1, $num2)  {
+    if($num2 === 0) return "error: no se puede dividir entre 0";
+    return $num1/$num2;
+    // ternaria
+    //return $num2===0? "error: no se puede dividir por 0" : $num1/$num2;
+
+}
+
+function calculadora($numero1,$numero2, $operacion){
+
+    switch ($operacion) {
+        case '+':
+           $resultado = sumar($numero1,$numero2);
+            break;
+        case '-':
+           $resultado = restar($numero1,$numero2);
+            break;
+        case '*':
+           $resultado = multiplicar($numero1,$numero2);
+            break;
+        case '/':
+           $resultado = dividir($numero1,$numero2);
+            break;
+        
+        default:
+           $resultado = "operación desconocida";
+            break;
+    }
+
+    return $resultado;
+}
+
+echo calculadora(5,0,'/');
