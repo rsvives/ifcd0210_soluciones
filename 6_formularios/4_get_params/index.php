e6f8d5a79412d2b0c839a9d5b69d652f040f8d29
Rodri <r.serranvives@gmail.com>
2025-03-27 15:39:39 +0100

    primer ejercicio

---
+++ 6_formularios/4_get_params/index.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+
+Crear un programa en php que reciba dos parametros por la URL num1 y num2
+en caso de que no se especifiquen, el programa mostrará el texto: "por favor, indique los parámetros correspondientes"
+y en caso de que se especifiquen mostrar la suma de ambos números utilizando una función que devuelva: "la suma es: X"
+
+
+Pista: la función isset($variable), nos permite comprobar si una variable está definida
+*/
