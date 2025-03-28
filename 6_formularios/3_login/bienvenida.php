d16b533e954fcdc044aafa15d20fbc2e811ed9b7
Rodri <r.serranvives@gmail.com>
2025-03-28 12:30:02 +0100

    ejercicio 3

--- 6_formularios/3_login/bienvenida.php
+++ 6_formularios/3_login/bienvenida.php
@@ -1,8 +1,15 @@
 <?php
 
-$usuario = $_GET['user'];
 //opcional: recibir el nombre del usuario por url
 //pista: añadir el parametro como parametro opcional de la url(nombre del fichero) en la función header("Location: ...")
+$usuario = '';
+if (isset($_GET['user'])) {
+    $usuario = $_GET['user'];
+}
+
+//alternativamente se puede hacer así (con una ternaria):
+// $usuario = isset($_GET['user']) ? $_GET['user'] : '';
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,9 +23,6 @@ $usuario = $_GET['user'];
 <body>
 
     <h1>Bienvenid@ usuario: <?= $usuario ?></h1>
-
-
-
 
 </body>
 
