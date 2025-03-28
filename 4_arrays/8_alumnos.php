4f7d68250373177c8dba05a8927f2a246f37d14c
Rodri <r.serranvives@gmail.com>
2025-03-28 15:42:57 +0100

    ejercicio alumnos

---
+++ 4_arrays/8_alumnos.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * 
+ * Dado el siguiente array de alumnos, realizar las siguientes funciones:
+ * 
+ * - Mostrar nombres: función que recibe el array y "pinta" un html con todos los nombres, notas y curso de los alumnos
+ * - Filtrar alumnos: funcion que recibe el array y un curso, y devuelve otro array con todos los alumnos que cumplen el criterio
+ * - Mostrar nota media: función que recibe un array de alumnos y devuelve la nota media
+ */
+
+$alumnos = [
+    ["nombre" => "Juan Pérez", "nota" => 8.5, "curso" => "1º"],
+    ["nombre" => "María López", "nota" => 9.2, "curso" => "2º"],
+    ["nombre" => "Carlos Sánchez", "nota" => 7.8, "curso" => "1º"],
+    ["nombre" => "Ana Torres", "nota" => 8.9, "curso" => "2º"],
+    ["nombre" => "Luis Fernández", "nota" => 6.5, "curso" => "1º"],
+    ["nombre" => "Sofía Ramírez", "nota" => 9.0, "curso" => "2º"],
+    ["nombre" => "Pedro Gómez", "nota" => 7.3, "curso" => "1º"],
+    ["nombre" => "Elena Duarte", "nota" => 8.7, "curso" => "2º"]
+];
