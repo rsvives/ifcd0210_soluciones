400f4ac4827c9caa7210d2efeda8c7523d61873f
Rodri <r.serranvives@gmail.com>
2025-03-31 15:14:08 +0200

    json

---
+++ 7_JSON/1_compra/index.php
@@ -0,0 +1,27 @@
+<?php
+
+function mostrarLista()
+{
+    //1) Leer contenido de compra.json ->file_get_contents('ruta_al_fichero') 
+    //tip: igualar una variable a la función para guardar el texto en dicha variable
+
+    //2) Convertir el contenido(string) a un array que pueda iterar -> json_decode($texto_del_json, true)
+
+    //3) Iterar el array renderizando (pintando) el html pertinente 
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
