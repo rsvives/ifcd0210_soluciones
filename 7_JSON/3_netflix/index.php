4fe67da4ee979ef44df431a0f0aaf796c5b0d484
rbercianos-class <rbifcd0210@gmail.com>
2025-04-01 17:13:07 +0200

    ejercicios extra

--- 7_JSON/3_netflix/index.php
+++ 7_JSON/3_netflix/index.php
@@ -5,3 +5,10 @@
 //convertir pelis.json a array
 
 //iterar y mostrar dicho array
+
+//Opcionales:
+
+// 1) añadir CSS (flexbos, formato tarjetitas, donde los géneros pueden ser badges)
+// 2) formulario de filtro por género  (pista: utilizar función in_array())
+// 3) Repetir el ejercicio pero sólo con JavaScript
+// 4) Filtrado dinámico de las películas
